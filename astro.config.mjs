79aa40b2dd2c18de2d416c0b9542cced1745a396
Lacy Pennell <lacy_pennell@Progressive.com>
2024-08-01 13:23:15 -0400

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
         },
         {
           label: 'Notes',
-          autogenerate: { directory: 'docs/notes' },
+          autogenerate: { directory: 'notes' },
         },
         {
           label: 'Reference',
